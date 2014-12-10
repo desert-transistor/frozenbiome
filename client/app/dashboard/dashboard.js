7b2b1daa72e8f9f122430cd8533a0e694d2dd0f9
Thomas Klonecke <tommyklon@gmail.com>
2014-12-10 13:30:47 -0800

    adds support for drag and drop posts on dash

--- client/app/dashboard/dashboard.js
+++ client/app/dashboard/dashboard.js
@@ -1,5 +1,62 @@
 angular.module('waffle.dashboard', ['masonry','ui.directives'])
 
+.directive('packery', ['$rootScope', '$timeout',
+  function($rootScope, $timeout) {
+    return {
+      restrict: 'A',
+      scope: true,
+      link: function(scope, element, attrs) {
+        console.log("link called on", element[0]);
+        scope.element = element;
+        if (!$rootScope.packery) {
+          $rootScope.packery = new Packery(element[0].parentElement, {
+            columnWidth: '.thumb',
+            itemSelector: '.thumb',
+            gutter: 10,
+          });
+
+          var draggable1 = new Draggabilly(element[0]);
+          $rootScope.packery.bindDraggabillyEvents(draggable1);
+
+          var orderItems = function() {
+            var itemElems = $rootScope.packery.getItemElements();
+            $(itemElems).each(function(i, itemElem) {
+              $(itemElem);
+            });
+          };
+
+          $rootScope.packery.on('layoutComplete', orderItems);
+          $rootScope.packery.on('dragItemPositioned', orderItems);
+
+
+        } else {
+          // console.log("else", element[0]);
+          $timeout(function() {
+            $rootScope.packery.appended(element[0])
+          });
+          var draggable2 = new Draggabilly(element[0]);
+          $rootScope.packery.bindDraggabillyEvents(draggable2);
+
+
+        }
+        $timeout(function() {
+          $rootScope.packery.layout();
+        });
+
+
+        // watch for destroying an item
+        scope.$on('$destroy', function() {
+          $rootScope.packery.remove(scope.element[0]);
+          scope.packery.layout();
+        });
+
+
+      }
+    };
+
+  }
+])
+
 .controller('DashboardController', function($scope, $rootScope, Dashboard, $timeout, $location, Auth, $stateParams) {
   $scope.posts = [];
   //For tracking which posts are already rendered
