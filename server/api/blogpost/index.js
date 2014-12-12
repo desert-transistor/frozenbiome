878b6fda88ef8893c51d50f6902a4f3c046416ac
Ashmeet Sidhu <ashmeet.sidhu7@gmail.com>
2014-12-11 19:15:54 -0800

    adds id to get blogposts

--- server/api/blogpost/index.js
+++ server/api/blogpost/index.js
@@ -4,6 +4,7 @@ var controller = require('./blogpost.controller');
 var router = express.Router();
 
 router.get('/', controller.index);
+router.get('/:id', controller.index);
 
 router.post('/:id', controller.update);
 router.post('/', controller.create);
