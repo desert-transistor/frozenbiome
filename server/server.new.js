31784f6c5199ffdbd6f8aaa08fc82dc4ea938d00
Ashmeet Sidhu <ashmeet.sidhu7@gmail.com>
2014-12-11 19:19:23 -0800

    fixes

---
+++ server/server.new.js
@@ -0,0 +1,21 @@
+// deps
+var mongoose = require('mongoose');
+var express = require('express');
+
+var config = require('./config');
+var router = require('./router');
+
+// connect to db
+mongoose.connect(config.mongo.url, config.mongo.options);
+
+// init app
+var app = express();
+
+// middlewares
+config.express(app);
+
+// router
+router(app);
+
+// expose app
+module.exports = app;
